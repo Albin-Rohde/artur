162f1edfd5327cc5944d0317b98571661cf0f312
Albin Rohde <albin.rohde@advinans.se>
2021-06-30 19:28:49 +0200

    wip: start frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	import CustomButton from "./components/CustomButton.svelte";
+	let count = 0;
+
+	function doWhenClick() {
+		count = count + 1;
+	}
+
+	function dontClick() {
+		count = count - 1;
+	}
+
+	let arrayOfImages = ['url1', 'ulr2', 'albin', 'dessi']
+
+
+	//username
+	//email
+	//password
+	//password confirmation
+</script>
+
+<main>
+	<h1>Clicked {count} many times!</h1>
+	<CustomButton doWhenClicked={doWhenClick} text="klicka på mig"/>
+	<CustomButton doWhenClicked={dontClick} text="klicka mig inte"/>
+	{#each arrayOfImages as imageUrl}
+		<p>{imageUrl}</p>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ frontend/src/components/CustomButton.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let doWhenClicked;
+  export let text;
+</script>
+
+<button on:click={doWhenClicked}>{text}</button>
+
+<style>
+  button {
+      background-color: red;
+  }
+</style>
