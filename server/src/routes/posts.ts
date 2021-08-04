f9face1a2fc1845e0546baa7caab6788e76118d4
Albin Rohde <albin.rohde@advinans.se>
2021-08-04 20:59:51 +0200

    3

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,1 +1,21 @@
+import {Router} from 'express';
+import multer from 'multer';
+import {Post} from "../entity/Post";
+import {User} from "../entity/User";
+import path from "path";
+
+const router = Router();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cv) => {
+    cv(null, 'post');
+  },
+  filename: async (req, file, cb) => {
+
+    const date = Date.now();
+    const inserts = await Post.create({
+      photoUrl: `${req.protocol}://${req.hostname}:5000/post/${date}/${path.extname(file.originalname)}`
+    })
+  },
+});
 
