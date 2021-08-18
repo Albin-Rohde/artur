a9a24dfd916d2337ab66b05c228312f5d7fcc1e8
Albin Rohde <albin.rohde@notifyme.se>
2021-08-18 18:33:57 +0200

    add initial svelte app to frontend

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,29 @@
+<div class='navbar'>
+  <div class="login">
+    <button class="login-btn">Log in</button>
+  </div>
+</div>
+<div class='line'></div>
+
+
+<style>
+  .navbar {
+    display: grid;
+    background-color: rgb(42, 42, 42);
+    grid-template-columns: 15% 70% 15%;
+    height: 7vh;
+  }
+  .login {
+    grid-column: 3;
+    float: right;
+    text-align: right;
+    margin-right: 10%;
+  }
+  .login-btn {
+    margin-top: 7%;
+  }
+  .line {
+    height: 0.1vh;
+    background-color: rgb(77, 77, 77);
+  }
+</style>
---
+++ frontend/src/views/Login/Login.svelte
@@ -0,0 +1,15 @@
+<script>
+  let email = ''
+  let password = ''
+</script>
+
+<h2>Login to your account</h2>
+
+<input type="text" placeholder="email" bind:value={email}><br>
+<input type="password" placeholder="password" bind:value={password}><br>
+<button>Login</button>
+
+
+<style>
+
+</style>
