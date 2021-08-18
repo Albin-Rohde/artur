0e0b73bfb2e01a1aae35e0494e71a19df3196ab3
Albin Rohde <albin.rohde@notifyme.se>
2021-08-18 18:33:57 +0200

    add initial svelte app to frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Navbar from './components/Navbar.svelte';
+
+	import Register from './views/Register/Register.svelte'
+</script>
+
+<main>
+	<Navbar/>
+	<Register/>
+	<p>I have an account, sign in</p>
+</main>
+
+<style>
+</style>
---
+++ frontend/src/views/Register/Register.svelte
@@ -0,0 +1,19 @@
+<script>
+  let username = '';
+  let email = '';
+  let password = '';
+</script>
+
+<h2>Register your account</h2>
+
+<input type="text" placeholder="username" bind:value={username}><br>
+<input type="email" placeholder="email" bind:value={email}><br>
+<input type="password" placeholder="password" bind:value={password}><br>
+<button>Create account</button><br>
+
+{username}<br>
+{email}<br>
+{password}
+<style>
+
+</style>
