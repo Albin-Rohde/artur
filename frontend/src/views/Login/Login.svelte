f18e8d3fb12179439c473c2aad51c8780c57c977
Soma Szabó <szabosonka@gmail.com>
2021-11-08 20:55:07 +0100

    Register and Login

--- frontend/src/views/Login/Login.svelte
+++ frontend/src/views/Login/Login.svelte
@@ -1,15 +1,45 @@
 <script>
-  let email = ''
-  let password = ''
+  let email = "";
+  let password = "";
 </script>
 
-<h2>Login to your account</h2>
+<div class="site-container">
+  <div class="login-container">
+    <h2>Login to your account</h2>
+    <br />
 
-<input type="text" placeholder="email" bind:value={email}><br>
-<input type="password" placeholder="password" bind:value={password}><br>
-
-
+    <input
+      class="input-style"
+      type="text"
+      placeholder="email"
+      bind:value={email}
+    /><br />
+    <input
+      class="input-style"
+      type="password"
+      placeholder="password"
+      bind:value={password}
+    /><br />
+    <p>I have an account, sign in</p>
+  </div>
+</div>
 
 <style>
+  .site-container {
+    padding: 0px;
+    margin: 0px;
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: 50px;
+  }
+  .login-container {
+    grid-column: 5/9;
+    grid-row: 6/10;
+  }
 
+  .input-style {
+    width: 100%;
+  }
 </style>
--- frontend/src/views/Register/Register.svelte
+++ frontend/src/views/Register/Register.svelte
@@ -1,21 +1,57 @@
 <script>
-import Button from "../../components/Button.svelte";
+  import Button from "../../components/Button.svelte";
 
-  let username = '';
-  let email = '';
-  let password = '';
+  let username = "";
+  let email = "";
+  let password = "";
 </script>
 
-<h2>Register your account</h2>
+<div class="site-container">
+  <div class="register-container">
+    <h2>Register your account</h2>
 
-<input type="text" placeholder="username" bind:value={username}><br>
-<input type="email" placeholder="email" bind:value={email}><br>
-<input type="password" placeholder="password" bind:value={password}><br>
-<Button text = 'Create Account'></Button>
+    <input
+      class="input-style"
+      type="text"
+      placeholder="username"
+      bind:value={username}
+    /><br />
+    <input
+      class="input-style"
+      type="email"
+      placeholder="email"
+      bind:value={email}
+    /><br />
+    <input
+      class="input-style"
+      type="password"
+      placeholder="password"
+      bind:value={password}
+    /><br />
+    <Button text="Create Account" />
 
-{username}<br>
-{email}<br>
-{password}
+    {username}<br />
+    {email}<br />
+    {password}
+  </div>
+</div>
+
 <style>
+  .site-container {
+    padding: 0px;
+    margin: 0px;
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: 50px;
+  }
+  .register-container {
+    grid-column: 5/9;
+    grid-row: 6/10;
+  }
 
+  .input-style {
+    width: 100%;
+  }
 </style>
