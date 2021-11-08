4499f8aea804466af032de7793323a288e2ec4a5
Soma Szabó <szabosonka@gmail.com>
2021-11-08 20:55:07 +0100

    Register and Login

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-  import Navbar from './components/Navbar.svelte';
+  import Navbar from "./components/Navbar.svelte";
 
-  import Register from './views/Register/Register.svelte'
+  import Register from "./views/Register/Register.svelte";
   import Login from "./views/Login/Login.svelte";
 </script>
 
 <main>
-	<Navbar/>
-	<Login/>
-	<p>I have an account, sign in</p>
+  <Navbar />
+  <!-- <Login /> -->
+  <Register />
 </main>
 
 <style>
+  * {
+    margin: 0px;
+    padding: 0px;
+  }
 </style>
