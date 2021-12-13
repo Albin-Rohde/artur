867cbc36d8025c130358e37b8e7df641f374ed41
Soma Szabó <szabosonka@gmail.com>
2021-12-13 18:57:22 +0100

    Added Dadhboard

---
+++ frontend/src/views/Dashboard/Dashboard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+</script>
+
+<div class="feedContainer">
+    <div class="feed"></div>
+
+</div>
+
+<style>
+.feedContainer{
+    display: grid;
+    grid-template-columns: repeat(12,1fr);  
+
+}
+.feed{
+    grid-column: 3/11;
+    grid-row: 2;
+
+}
+</style>
