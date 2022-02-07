1f072a85a17871e782844dc168e8c6783172d2e4
Axel Burling <axel.burling@gmail.com>
2022-02-07 20:28:29 +0100

    adding feature: ratelimiting, add follower and search user

--- frontend/src/components/PostCreate.svelte
+++ frontend/src/components/PostCreate.svelte
@@ -1,174 +1,187 @@
 <script lang="ts">
+  // import Dropzone from 'svelte-file-dropzone';
+  import Dropzone from "svelte-atoms/DropZone.svelte";
+  import type { IUser } from "../api-client";
+  import { Post } from "../api-client";
+  export let currentUser: IUser;
+  export let onClick: (...a: any) => any;
 
+  let post_description: string;
+  let post_title: string;
+  let file: File;
+  let file_name: string;
 
-// import Dropzone from 'svelte-file-dropzone';
-import Dropzone from 'svelte-atoms/DropZone.svelte';
-import type { IUser } from "../api-client";
-import { Post } from "../api-client";
-export let currentUser: IUser;
-export let onClick: (...a: any) => any;
-
-
-let post_description: string;
-let post_title: string;
-let file: File;
-let file_name: string
-
-let reader = new FileReader()
-const post = new Post();
-(async ()=> {
-    console.log("hello")
-    const posts = await post.getFeed('color')
-    console.log(posts)
-})()
-const upload = async () => {
+  let reader = new FileReader();
+  const post = new Post();
+  (async () => {
+    console.log("hello");
+    const posts = await post.getFeed("color");
+    console.log(posts);
+  })();
+  const upload = async () => {
     console.log(post_description, post_title, file[0]);
-    const p =  await post.upload({
-        post_description,
-        post_title,
-        file: file
-    })
-    console.log(p)
-	window.location.replace("/");
-}
-let image;
+    const p = await post.upload({
+      post_description,
+      post_title,
+      file: file,
+    });
+    console.log(p);
+    window.location.replace("/");
+  };
+  let image;
 
-const fileChose = (e) => {
-	console.log(e.dataTransfer.files)
-	const f = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
-	console.log(f)
-	if(f){
-	reader.addEventListener('load', () => {
-		image = reader.result;
-	})
-	reader.readAsDataURL(f);
-	file = f
-	file_name = f.name
-	}
-}
+  const fileChose = (e) => {
+    console.log(e.dataTransfer.files);
+    const f = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    console.log(f);
+    if (f) {
+      reader.addEventListener("load", () => {
+        image = reader.result;
+      });
+      reader.readAsDataURL(f);
+      file = f;
+      file_name = f.name;
+    }
+  };
 
-
-let topDiv
-let visible=false
-let prevOnTop
-let closeCallback
-
-
-	
+  let topDiv;
+  let visible = false;
+  let prevOnTop;
+  let closeCallback;
 </script>
-	<div class="background" on:click|preventDefault={onClick}>
-	</div>
-		<div class="container">
 
-			<form class="form" action="">
-				<textarea class="title" type="text" name="title" bind:value={post_title} placeholder="Title"></textarea>
-				<textarea class="description" type="text" name="content" bind:value={post_description} placeholder="Content"></textarea>
-				<!-- <Dropzone on:drop={fileChose} /> -->
-				<Dropzone class="drop" fileTitle={file_name} dropOnPage on:drop={fileChose} on:change={fileChose}  />
-				{#if image}
-					<img src={image} alt="hello" class="file"  />
-				{/if}
-				<button class='knapp' on:click|preventDefault={()=> upload()} >{"Create Post"} </button>
-				<button class="close" on:click|preventDefault={onClick} >Cancel</button>
-			</form>
-		</div>
-	
+<div class="background" on:click|preventDefault={onClick} />
+<div class="container">
+  <form class="form" action="">
+    <textarea
+      class="title"
+      type="text"
+      name="title"
+      bind:value={post_title}
+      placeholder="Title"
+    />
+    <textarea
+      class="description"
+      type="text"
+      name="content"
+      bind:value={post_description}
+      placeholder="Content"
+    />
+    <!-- <Dropzone on:drop={fileChose} /> -->
+    <Dropzone
+      class="drop"
+      fileTitle={file_name}
+      dropOnPage
+      on:drop={fileChose}
+      on:change={fileChose}
+    />
+    {#if image}
+      <img src={image} alt="hello" class="file" />
+    {/if}
+    <button class="knapp" on:click|preventDefault={() => upload()}
+      >{"Create Post"}
+    </button>
+    <button class="close" on:click|preventDefault={onClick}>Cancel</button>
+  </form>
+</div>
+
 <style>
-	textarea{
-		resize: none;
-	}
+  textarea {
+    resize: none;
+  }
 
-	.drop{
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
+  .drop {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-	.background{
-		position: fixed;
-		left: 0px;
-		bottom: 0px;
-		width: 100vw;
-		height: 100vh;
-		background-color: rgba(32, 32, 32, 0.7);
-		z-index: 10;
-	}	
-	.container{
-		position: fixed;
-		left: 5%;
-		width: 90vw;
-		height: 90vh;
-		bottom: 5%;
-        margin: 0 auto;
-		border-radius: 100px;
+  .background {
+    position: fixed;
+    left: 0px;
+    bottom: 0px;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(32, 32, 32, 0.7);
+    z-index: 10;
+  }
+  .container {
+    position: fixed;
+    left: 5%;
+    width: 90vw;
+    height: 90vh;
+    bottom: 5%;
+    margin: 0 auto;
+    border-radius: 100px;
 
-		background-color: #add5a9;
-		z-index: 20;
-	}
-	.form{
-		height: 100%;
-		width: 100%;
-		display: grid;
-		grid-template-columns: repeat(12,1fr);
-		grid-template-rows: repeat(12,1fr);
-	}
+    background-color: #add5a9;
+    z-index: 20;
+  }
+  .form {
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-rows: repeat(12, 1fr);
+  }
 
-	.title{
-		grid-row: 2/3;
-		grid-column: 8/11;
-		background-color: white;
-		border-radius: 30px;	
-		padding: 1rem;
-
-	}
-	.description{
-		grid-row: 3/6;
-		grid-column: 8/11;
-		background-color: white;
-		border-radius: 30px;	
-		padding: 1rem;
-	}
-	.file{
-		width: 100%;
-		height: 100%;
-		grid-row: 2/12;
-		grid-column: 2/7;
-		background-color: white;
-		border-radius: 30px;	
-	}
-	.knapp {
-		overflow: visible;
-		position: absolute;
-		cursor:pointer;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 50px;
-        background: white;
-		bottom: 5%;
-		right:17%;
-        width: 10%;
-        height: 5%;
-    }
-    .knapp:hover, .close:hover{
-        background: rgb(219, 219, 219);
-    }
-    .knapp:focus, .close:focus {
-        background: #777676;
-    }
-	.close{
-		overflow: visible;
-		position: absolute;
-		display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 50px;
-        background: white;
-		height: 5%;
-		width: 10%;
-		bottom: 5%;
-		right:28%;
-		cursor: pointer;
-		fill:#97445e;
-	}
+  .title {
+    grid-row: 2/3;
+    grid-column: 8/11;
+    background-color: white;
+    border-radius: 30px;
+    padding: 1rem;
+  }
+  .description {
+    grid-row: 3/6;
+    grid-column: 8/11;
+    background-color: white;
+    border-radius: 30px;
+    padding: 1rem;
+  }
+  .file {
+    width: 100%;
+    height: 100%;
+    grid-row: 2/12;
+    grid-column: 2/7;
+    background-color: white;
+    border-radius: 30px;
+  }
+  .knapp {
+    overflow: visible;
+    position: absolute;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50px;
+    background: white;
+    bottom: 5%;
+    right: 17%;
+    width: 10%;
+    height: 5%;
+  }
+  .knapp:hover,
+  .close:hover {
+    background: rgb(219, 219, 219);
+  }
+  .knapp:focus,
+  .close:focus {
+    background: #777676;
+  }
+  .close {
+    overflow: visible;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50px;
+    background: white;
+    height: 5%;
+    width: 10%;
+    bottom: 5%;
+    right: 28%;
+    cursor: pointer;
+    fill: #97445e;
+  }
 </style>
--- frontend/src/views/Dashboard/Dashboard.svelte
+++ frontend/src/views/Dashboard/Dashboard.svelte
@@ -1,93 +1,90 @@
 <script lang="ts">
-    import type { IPost,IUser } from "../../api-client";
-    import { Post,User } from "../../api-client";
-    import Button from "../../components/Button.svelte";
-    import PlusButton from "../../components/PlusButton.svelte";
-    import PostCreate from "../../components/PostCreate.svelte";
-    export let currentUser: IUser;
-    export let onLogout: (u: IUser) => Promise<void>
-    console.log(currentUser);
-    const scrollY = document.body.style.top;
-    let postCreation = ""
-    let post_description: string;
-    let post_title: string;
-    let file: FileList;
-    let posts: IPost[] = [];
-    
-    const user = new User()
+  import type { IUser } from "../../api-client";
+  import { Post, User } from "../../api-client";
+  import type { IPost } from "../../api-client";
+  import PlusButton from "../../components/PlusButton.svelte";
+  import Button from "../../components/Button.svelte";
+  import PostCreate from "../../components/PostCreate.svelte";
+  export let currentUser: IUser;
+  export let onLogout: (u: IUser) => Promise<void>;
+  console.log(currentUser);
+  const scrollY = document.body.style.top;
+  let postCreation = "";
+  let post_description: string;
+  let post_title: string;
+  let file: FileList;
+  let posts: IPost[] = [];
 
-    const showPostCreate = ()=>{
-        postCreation =  "Visible"
-        document.body.style.position = 'fixed';
-        document.body.style.top = `-${window.scrollY}px`;
-    }
-    const hidePostCreate = ()=>{
-        postCreation =  ""
-        document.body.style.position = '';
-        document.body.style.top = '';
-    }
+  const user = new User();
 
-    const uploadAvatar = async () => {
-        console.log(file[0])
-        const avatar = await user.uploadAvatar(file[0])
-        console.log(avatar)
-    }
+  const showPostCreate = () => {
+    postCreation = "Visible";
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${window.scrollY}px`;
+  };
+  const hidePostCreate = () => {
+    postCreation = "";
+    document.body.style.position = "";
+    document.body.style.top = "";
+  };
 
- (async ()=> {
-     console.log("currentUser", currentUser);
-    console.log("hello")
+  const uploadAvatar = async () => {
+    console.log(file[0]);
+    const avatar = await user.uploadAvatar(file[0]);
+    console.log(avatar);
+  };
+
+  (async () => {
+    console.log("currentUser", currentUser);
+    console.log("hello");
     const postClient = new Post();
-    posts = await postClient.getFeed('time')
+    posts = await postClient.getFeed("time");
     console.log(posts);
- })()
-
-
+  })();
 </script>
 
 <h1>Dethär är din dashboard.</h1>
-<h2> Hej {currentUser.name}.</h2>
+<h2>Hej {currentUser.name}.</h2>
 
-<Button onClick={() => onLogout(currentUser)} text="Logout"/>
+<Button onClick={() => onLogout(currentUser)} text="Logout" />
 
 {#if postCreation === "Visible"}
-    <PostCreate onClick={()=>hidePostCreate()} currentUser = {currentUser}/>
+  <PostCreate onClick={() => hidePostCreate()} {currentUser} />
 {:else}
-    <PlusButton onClick={()=> showPostCreate()}/>
+  <PlusButton onClick={() => showPostCreate()} />
 {/if}
-    
-   
+
 <div class="feedContainer">
-    <input type="file" bind:files={file} />
-    <button on:click|preventDefault={() => uploadAvatar()}>Click me</button>
-    <div class="feed">
-        {#if posts.length > 0}
-            {#each posts as post}
-                <div style="background-image: url({post.photoUrl});background-size: auto; background-repeat: no-repeat;"></div>
-            {/each}
-        {/if}
-    </div>
-    <!--<input type="text" name="search" bind:value={search} on:input={() => {searchUser()}}  WIP: -->
+  <input type="file" bind:files={file} />
+  <button on:click|preventDefault={() => uploadAvatar()}>Click me</button>
+  <div class="feed">
+    {#if posts.length > 0}
+      {#each posts as post}
+        <div
+          style="background-image: url({post.photoUrl});background-size: auto; background-repeat: no-repeat;"
+        />
+      {/each}
+    {/if}
+  </div>
+  <!--<input type="text" name="search" bind:value={search} on:input={() => {searchUser()}}  WIP: -->
 </div>
 
 <style>
-.feedContainer{
+  .feedContainer {
     display: grid;
-    grid-template-columns: repeat(12,1fr);  
-
-}
-.feed{
+    grid-template-columns: repeat(12, 1fr);
+  }
+  .feed {
     grid-column: 3/11;
     grid-row: 2;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     gap: 1%;
     /* grid-auto-rows: minmax(50px, auto); */
-
-}
-.feed div{
+  }
+  .feed div {
     width: 100%;
     height: 200px;
     /*background-color: #d6a7bd;*/
-}
-
+  }
 </style>
