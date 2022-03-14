098f46b28af79b0393e5d0e77da5e1d5c5f6e4de
Soma Szabó <szabosonka@gmail.com>
2022-03-14 18:17:36 +0100

    changes to dashboard and profile

--- frontend/src/views/Dashboard/Dashboard.svelte
+++ frontend/src/views/Dashboard/Dashboard.svelte
@@ -5,6 +5,7 @@
   import PlusButton from "../../components/PlusButton.svelte";
   import Button from "../../components/Button.svelte";
   import PostCreate from "../../components/PostCreate.svelte";
+  import Feed from "../../components/Feed.svelte";
   export let currentUser: IUser;
   export let onLogout: (u: IUser) => Promise<void>;
   console.log(currentUser);
@@ -14,7 +15,6 @@
   let post_title: string;
   let file: FileList;
   let posts: IPost[] = [];
-
   const user = new User();
 
   const showPostCreate = () => {
@@ -54,37 +54,4 @@
   <PlusButton onClick={() => showPostCreate()} />
 {/if}
 
-<div class="feedContainer">
-  <input type="file" bind:files={file} />
-  <button on:click|preventDefault={() => uploadAvatar()}>Click me</button>
-  <div class="feed">
-    {#if posts.length > 0}
-      {#each posts as post}
-        <div
-          style="background-image: url({post.photoUrl});background-size: auto; background-repeat: no-repeat;"
-        />
-      {/each}
-    {/if}
-  </div>
-  <!--<input type="text" name="search" bind:value={search} on:input={() => {searchUser()}}  WIP: -->
-</div>
-
-<style>
-  .feedContainer {
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
-  }
-  .feed {
-    grid-column: 3/11;
-    grid-row: 2;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 1%;
-    /* grid-auto-rows: minmax(50px, auto); */
-  }
-  .feed div {
-    width: 100%;
-    height: 200px;
-    /*background-color: #d6a7bd;*/
-  }
-</style>
+<Feed feedType="time" />
--- frontend/src/views/Profile/Profile.svelte
+++ frontend/src/views/Profile/Profile.svelte
@@ -1,13 +1,77 @@
 <script lang="ts">
-  import type { IUser } from "../../api-client";
-
+  import Feed from "../../components/Feed.svelte";
+  import type { IPost, IUser } from "../../api-client";
+  import PostCreate from "../../components/PostCreate.svelte";
+  import PlusButton from "../../components/PlusButton.svelte";
   export let currentUser: IUser;
-  export let onLogout: (u: IUser) => Promise<void>;
+  export let onLogout: () => Promise<void>;
+  let postCreation = "";
+  const showPostCreate = () => {
+    postCreation = "Visible";
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${window.scrollY}px`;
+  };
+  const hidePostCreate = () => {
+    postCreation = "";
+    document.body.style.position = "";
+    document.body.style.top = "";
+  };
 </script>
 
+<br />
 <div class="profileContainer">
-  <div class="profile" />
+  <div class="profile">
+    <div class="profilePicture">
+      <div />
+    </div>
+    <div class="profileMain">
+      <h2>{currentUser.name}</h2>
+      <p>
+        Hi! I'm Many. <br />
+        xe/xim <br />
+        Blue Lives Matter
+      </p>
+    </div>
+  </div>
 </div>
+<br />
+
+{#if postCreation === "Visible"}
+  <PostCreate onClick={() => hidePostCreate()} {currentUser} />
+{:else}
+  <PlusButton onClick={() => showPostCreate()} />
+{/if}
+
+<Feed feedType="time" />
 
 <style>
+  .profileContainer {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+  }
+  .profile {
+    height: 15vh;
+    grid-column: 3/11;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+  }
+  .profilePicture {
+    grid-column: 2/4;
+  }
+
+  .profilePicture > div {
+    width: 15vh;
+    height: 100%;
+    border-radius: 50%;
+    background-size: cover;
+    background-image: url("https://preview.redd.it/1nox7mat7l781.jpg?auto=webp&s=89ccd4638a8c1452c0000bd884d18e0366ee1232");
+  }
+
+  .profileMain {
+    grid-column: 5/13;
+  }
+
+  .profileMain > h2 {
+    text-align: center;
+  }
 </style>
